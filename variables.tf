424857fb541fdd6cca849d73cc9b03e5356375c8
ROHIT6350 <rohitjadhav6350@gmail.com>
2025-10-05 15:43:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
-variable "docker_username" {
-  description = "Docker Hub username"
+variable "server_ip" {
+  description = "192.168.0.133"
+  type        = string
+}
+
+variable "server_user" {
+  description = "rohit"
   type        = string
 }
