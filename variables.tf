69787d836fbde1f94c75510fdc52dd9ad9e03174
ROHIT6350 <rohitjadhav6350@gmail.com>
2025-10-05 17:03:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,17 @@
 variable "server_ip" {
-  description = "192.168.0.133"
+  description = "Target server IP address"
   type        = string
+  default     = "192.168.0.132"
 }
 
 variable "server_user" {
-  description = "rohit"
+  description = "SSH username"
   type        = string
+  default     = "rohit"
+}
+
+variable "private_key_path" {
+  description = "Path to SSH private key"
+  type        = string
+  default     = "/home/rohit/.ssh/id_rsa"
 }
